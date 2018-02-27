9c9a4cfb2093273281ab355d99eb95feb085c117
Thomas <tlabrux@gmail.com>
2018-02-28 00:47:00 +0100

    Affichage du graph avec d3.v4

--- js/script_JsNetworkx.js
+++ js/script_JsNetworkx.js
@@ -37,9 +37,117 @@ var G = new jsnx.Graph();
             }
 
             //G.addNodesFrom(idList);
-console.log(idList);
-console.log(idEdge);
-var width = 1920, height = 1080
+
+
+
+var svg = d3v4.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+var color = d3v4.scaleOrdinal(d3v4.schemeCategory20);
+
+var simulation = d3v4.forceSimulation()
+    .force("link", d3v4.forceLink().id(function(d) { return d.id; }))
+    .force("charge", d3v4.forceManyBody())
+    .force("center", d3v4.forceCenter(width / 2, height / 2));
+
+
+d3v4.json("Donnees/BB_cum_D3.json", function(error, graph) {
+  if (error) throw error;
+
+  var link = svg.append("g")
+      .attr("class", "links")
+    .selectAll("line")
+    .data(graph.links)
+    .enter().append("line")
+      .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+  var node = svg.append("g")
+      .attr("class", "nodes")
+    .selectAll("circle")
+    .data(graph.nodes)
+    .enter().append("circle")
+      .attr("r", 10)
+      .attr("fill", function(d) { return color(d.group); })
+      .call(d3v4.drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended));
+
+
+  node.append("title")
+      .text(function(d) { return d.label; });
+
+  node.append("image")
+    .attr("xlink:href", function(d) { return d.image; });
+
+  node.append("text")
+      .text(function(d) { return d.label; });
+
+   node.append("id")
+       .text(function(d) { return d.id; });
+
+    node.on("click", function()
+    {
+      var path_image = "<img src=\"Donnees/Photos/BB/Walter_White.jpg\" alt=\"Image\">";
+      var id = this.childNodes[3].innerHTML;
+      var node_image = this.childNodes[1].href.baseVal;
+      var name = this.childNodes[2].innerHTML;
+
+      if (node_image === "")
+      {
+        default_image = "Donnees/Photos/Default.png";
+        document.getElementById('img').attributes[1].value = default_image;
+      }
+      else
+      {
+        image = node_image
+        document.getElementById('img').attributes[1].value = image;
+      }
+
+      document.getElementById('name').innerHTML = name;
+    });
+
+  simulation
+      .nodes(graph.nodes)
+      .on("tick", ticked);
+
+  simulation.force("link")
+      .links(graph.links);
+
+  function ticked() {
+    link
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  }
+});
+
+function dragstarted(d) {
+  if (!d3v4.event.active) simulation.alphaTarget(0.3).restart();
+  d.fx = d.x;
+  d.fy = d.y;
+}
+
+function dragged(d) {
+  d.fx = d3v4.event.x;
+  d.fy = d3v4.event.y;
+}
+
+function dragended(d) {
+  if (!d3v4.event.active) simulation.alphaTarget(0);
+  d.fx = null;
+  d.fy = null;
+}
+
+
+
+/*var width = 1920, height = 1080
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
