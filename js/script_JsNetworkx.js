f662af690f2b5accc16e421deca5b2cf2e2172d2
Thomas <tlabrux@gmail.com>
2018-02-28 01:34:46 +0100

    finalisation affichage avec d3v4

--- js/script_JsNetworkx.js
+++ js/script_JsNetworkx.js
@@ -1,227 +1,238 @@
 var G = new jsnx.Graph();
-        var idList = [];
-        var idEdge = [];
+var idList = [];
+var idEdge = [];
 
-        function loadJSON(callback) {
-            var xobj = new XMLHttpRequest();
-            xobj.overrideMimeType("application/json");
-            xobj.open('GET', 'Donnees/joder.json', true);
-            xobj.onreadystatechange = function() {
-                if (xobj.readyState == 4 && xobj.status == "200") {
-                    // .open will NOT return a value but simply returns undefined in async mode so use a callback
-                    callback(xobj.responseText);
-                }
-            }
-            xobj.send(null);
+function loadJSON(callback) {
+	var xobj = new XMLHttpRequest();
+	xobj.overrideMimeType("application/json");
+	xobj.open('GET', 'Donnees/BB_cum_D3.json', true);
+	xobj.onreadystatechange = function() {
+		if (xobj.readyState == 4 && xobj.status == "200") {
+			// .open will NOT return a value but simply returns undefined in async mode so use a callback
+			callback(xobj.responseText);
+		}
+	}
+	xobj.send(null);
 
-        }
+}
 
-        // Call to function with anonymous callback
-
-
-        loadJSON(function(response) {
-            // Do Something with the response e.g.
-            jsonresponse = JSON.parse(response);
-           
- 
-            for (var i = jsonresponse.nodes.length - 1; i >= 0; i--) {
-                idList.push(jsonresponse.nodes[i]);
-                //console.log(jsonresponse.nodes[i]);
-            }
+// Call to function with anonymous callback
 
 
-            for (var i = jsonresponse.links.length - 1; i >= 0; i--) {
-                idEdge.push(jsonresponse.links[i]);
-                //console.log(jsonresponse.links[i])
-                //G.addEdge(jsonresponse.links[i].source, jsonresponse.links[i].target);
-            }
+loadJSON(function(response) {
+	// Do Something with the response e.g.
+	jsonresponse = JSON.parse(response);
 
-            //G.addNodesFrom(idList);
+
+	for (var i = jsonresponse.nodes.length - 1; i >= 0; i--) {
+		idList.push(jsonresponse.nodes[i]);
+		//console.log(jsonresponse.nodes[i]);
+	}
+	G.addNodesFrom(idList);
+
+	for (var i = jsonresponse.links.length - 1; i >= 0; i--) {
+		idEdge.push(jsonresponse.links[i]);
+		//console.log(jsonresponse.links[i])
+		G.addEdge(jsonresponse.links[i].source, jsonresponse.links[i].target);
+	}
 
 
 
-var svg = d3v4.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
-
-var color = d3v4.scaleOrdinal(d3v4.schemeCategory20);
-
-var simulation = d3v4.forceSimulation()
-    .force("link", d3v4.forceLink().id(function(d) { return d.id; }))
-    .force("charge", d3v4.forceManyBody())
-    .force("center", d3v4.forceCenter(width / 2, height / 2));
 
 
-d3v4.json("Donnees/BB_cum_D3.json", function(error, graph) {
-  if (error) throw error;
 
-  var link = svg.append("g")
-      .attr("class", "links")
-    .selectAll("line")
-    .data(graph.links)
-    .enter().append("line")
-      .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
-
-  var node = svg.append("g")
-      .attr("class", "nodes")
-    .selectAll("circle")
-    .data(graph.nodes)
-    .enter().append("circle")
-      .attr("r", 10)
-      .attr("fill", function(d) { return color(d.group); })
-      .call(d3v4.drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended));
+	//---------- AFFICHAGE D3 ----------
 
 
-  node.append("title")
-      .text(function(d) { return d.label; });
+	var svg = d3v4.select("svg"),
+	width = +svg.attr("width"),
+	height = +svg.attr("height");
 
-  node.append("image")
-    .attr("xlink:href", function(d) { return d.image; });
+	var color = d3v4.scaleOrdinal(d3v4.schemeCategory20);
 
-  node.append("text")
-      .text(function(d) { return d.label; });
+	var simulation = d3v4.forceSimulation()
+	.force("link", d3v4.forceLink().id(function(d) { return d.id; }))
+	.force("charge", d3v4.forceManyBody())
+	.force("center", d3v4.forceCenter(width / 2, height / 2));
 
-   node.append("id")
-       .text(function(d) { return d.id; });
 
-    node.on("click", function()
-    {
-      var path_image = "<img src=\"Donnees/Photos/BB/Walter_White.jpg\" alt=\"Image\">";
-      var id = this.childNodes[3].innerHTML;
-      var node_image = this.childNodes[1].href.baseVal;
-      var name = this.childNodes[2].innerHTML;
+	var svg = d3v4.select("svg"),
+	width = +svg.attr("width"),
+	height = +svg.attr("height");
 
-      if (node_image === "")
-      {
-        default_image = "Donnees/Photos/Default.png";
-        document.getElementById('img').attributes[1].value = default_image;
-      }
-      else
-      {
-        image = node_image
-        document.getElementById('img').attributes[1].value = image;
-      }
+	var color = d3v4.scaleOrdinal(d3v4.schemeCategory20);
 
-      document.getElementById('name').innerHTML = name;
-    });
+	var simulation = d3v4.forceSimulation()
+	.force("link", d3v4.forceLink().id(function(d) { return d.id; }))
+	.force("charge", d3v4.forceManyBody())
+	.force("center", d3v4.forceCenter(width / 2, height / 2));
 
-  simulation
-      .nodes(graph.nodes)
-      .on("tick", ticked);
+	var link = svg.append("g")
+	.attr("class", "links")
+	.selectAll("line")
+	.data(idEdge)
+	.enter().append("line")
+	.attr("stroke-width", function(d) { return Math.sqrt(d.value); });
 
-  simulation.force("link")
-      .links(graph.links);
+	var node = svg.append("g")
+	.attr("class", "nodes")
+	.selectAll("circle")
+	.data(idList)
+	.enter().append("circle")
+	.attr("r", 10)
+	.attr("fill", function(d) { return color(d.group); })
+	.call(d3v4.drag()
+		.on("start", dragstarted)
+		.on("drag", dragged)
+		.on("end", dragended));
 
-  function ticked() {
-    link
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
 
-    node
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-  }
+	node.append("title")
+	.text(function(d) { return d.label; });
+
+	node.append("image")
+	.attr("xlink:href", function(d) { return d.image; });
+
+	node.append("text")
+	.text(function(d) { return d.label; });
+
+	node.append("id")
+	.text(function(d) { return d.id; });
+
+	node.on("click", function()
+	{
+		var path_image = "<img src=\"Donnees/Photos/BB/Walter_White.jpg\" alt=\"Image\">";
+		var id = this.childNodes[3].innerHTML;
+		var node_image = this.childNodes[1].href.baseVal;
+		var name = this.childNodes[2].innerHTML;
+
+		if (node_image === "")
+		{
+			default_image = "Donnees/Photos/Default.png";
+			document.getElementById('img').attributes[1].value = default_image;
+		}
+		else
+		{
+			image = node_image
+			document.getElementById('img').attributes[1].value = image;
+		}
+
+		document.getElementById('name').innerHTML = name;
+	});
+
+	simulation
+	.nodes(idList)
+	.on("tick", ticked);
+
+	simulation.force("link")
+	.links(idEdge);
+
+	function ticked() {
+		link
+		.attr("x1", function(d) { return d.source.x; })
+		.attr("y1", function(d) { return d.source.y; })
+		.attr("x2", function(d) { return d.target.x; })
+		.attr("y2", function(d) { return d.target.y; });
+
+		node
+		.attr("cx", function(d) { return d.x; })
+		.attr("cy", function(d) { return d.y; });
+	}
+
+	function dragstarted(d) {
+		if (!d3v4.event.active) simulation.alphaTarget(0.3).restart();
+		d.fx = d.x;
+		d.fy = d.y;
+	}
+
+	function dragged(d) {
+		d.fx = d3v4.event.x;
+		d.fy = d3v4.event.y;
+	}
+
+	function dragended(d) {
+		if (!d3v4.event.active) simulation.alphaTarget(0);
+		d.fx = null;
+		d.fy = null;
+	}
 });
-
-function dragstarted(d) {
-  if (!d3v4.event.active) simulation.alphaTarget(0.3).restart();
-  d.fx = d.x;
-  d.fy = d.y;
-}
-
-function dragged(d) {
-  d.fx = d3v4.event.x;
-  d.fy = d3v4.event.y;
-}
-
-function dragended(d) {
-  if (!d3v4.event.active) simulation.alphaTarget(0);
-  d.fx = null;
-  d.fy = null;
-}
-
 
 
 /*var width = 1920, height = 1080
 
 var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+		.attr("width", width)
+		.attr("height", height);
 
 var force = d3.layout.force()
-    .gravity(0.05)
-    .distance(100)
-    .charge(-100)
-    .size([width, height])
-    .nodes(idList)
-    .links(idEdge)
-    .start();
+		.gravity(0.05)
+		.distance(100)
+		.charge(-100)
+		.size([width, height])
+		.nodes(idList)
+		.links(idEdge)
+		.start();
 
-  var link = svg.selectAll(".link")
-      .data(idEdge)
-    .enter().append("line")
-      .attr("class", "link");
+	var link = svg.selectAll(".link")
+			.data(idEdge)
+		.enter().append("line")
+			.attr("class", "link");
 
-  var node = svg.selectAll(".node")
-      .data(idList)
-    .enter().append("g")
-      .attr("class", "node")
-      .call(force.drag);
+	var node = svg.selectAll(".node")
+			.data(idList)
+		.enter().append("g")
+			.attr("class", "node")
+			.call(force.drag);
 
-    node.append("image")
-      //.attr("xlink:href", "https://github.com/favicon.ico")
-      .attr("x", -8)
-      .attr("y", -8)
-      .attr("width", 16)
-      .attr("height", 16);
+		node.append("image")
+			//.attr("xlink:href", "https://github.com/favicon.ico")
+			.attr("x", -8)
+			.attr("y", -8)
+			.attr("width", 16)
+			.attr("height", 16);
 
-    node.append("text")
-      .attr("dx", 12)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.name });
+		node.append("text")
+			.attr("dx", 12)
+			.attr("dy", ".35em")
+			.text(function(d) { return d.name });
 
-    node.append("circle")
-        .attr("r", 15);
+		node.append("circle")
+				.attr("r", 15);
 
 
-  force.on("tick", function() {
-    link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+	force.on("tick", function() {
+		link.attr("x1", function(d) { return d.source.x; })
+				.attr("y1", function(d) { return d.source.y; })
+				.attr("x2", function(d) { return d.target.x; })
+				.attr("y2", function(d) { return d.target.y; });
 
-    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+		node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
-});
+	});*/
 
-           /* jsnx.draw(G, {
-                element: '#canvas',
-                layoutAttr: {
-                    charge: -120,
-                    linkDistance: 20
-                },
-                nodeAttr: {
-                    r: function(d){*/
-                        /*if(G.neighbors(d.node).length>10){return 30;} 
-                        else if(G.neighbors(d.node).length>5){return 20} 
-                        else{return 10;}*/
-                        /*return (G.neighbors(d.node).length)*3},
-                    title: function(d) { return d.node;}
-                },
-                nodeStyle: {
-                    fill : function(d){
-                        if(G.neighbors(d.node).length>10){return 'red';} 
-                        else if(G.neighbors(d.node).length>5){return 'blue'} 
-                        else{return 'yellow';}},
-                },
-                    stroke: 'none',
-                edgeStyle: {
-                    fill: '#999'
-                }
-            });*/
-});
+					 /* jsnx.draw(G, {
+								element: '#canevas',
+								layoutAttr: {
+										charge: -120,
+										linkDistance: 20
+								},
+								nodeAttr: {
+									r: function(d){
+												if(G.neighbors(d.node).length>10){return 30;} 
+												else if(G.neighbors(d.node).length>5){return 20} 
+													else{return 10;}
+												return (G.neighbors(d.node).length)*3},
+										title: function(d) { return d.node;}
+								},
+								nodeStyle: {
+										fill : function(d){
+												if(G.neighbors(d.node).length>10){return 'red';} 
+												else if(G.neighbors(d.node).length>5){return 'blue'} 
+												else{return 'yellow';}},
+								},
+										stroke: 'none',
+								edgeStyle: {
+										fill: '#999'
+								}
+							});
+						});*/
