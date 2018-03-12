494a60771d8c7b0920532175c60751e6d012189e
Gwénola BIERMÉ <gwen.br@sfr.fr>
2018-03-12 17:53:10 +0100

    Suppression des erreurs sur les résumés

--- js/scriptSiteWeb.js
+++ js/scriptSiteWeb.js
@@ -1,44 +1,87 @@
 function getResumeSaison(saison)
 {
+  var chemin = document.location.pathname;
+  var fichierHTML = chemin.substring(chemin.lastIndexOf( "/" )+1);
+
   var saison_ = saison.firstChild.nodeValue;
-  if(saison_ === " Saison 1 ")
+
+  if(fichierHTML === "BB.html")
   {
-    document.getElementById("resume1").attributes[1].value = "visible";
-    document.getElementById("resume2").attributes[1].value = "hidden";
-    document.getElementById("resume3").attributes[1].value = "hidden";
-    document.getElementById("resume4").attributes[1].value = "hidden";
-    document.getElementById("resume5").attributes[1].value = "hidden";
+    if(saison_ === " Saison 1 ")
+    {
+      document.getElementById("resume1").attributes[1].value = "visible";
+      document.getElementById("resume2").attributes[1].value = "hidden";
+      document.getElementById("resume3").attributes[1].value = "hidden";
+    }
+    else if(saison_ === " Saison 2 ")
+    {
+      document.getElementById("resume2").attributes[1].value = "visible";
+      document.getElementById("resume1").attributes[1].value = "hidden";
+      document.getElementById("resume3").attributes[1].value = "hidden";
+    }
+    else if(saison_ === " Saison 3 ")
+    {
+      document.getElementById("resume3").attributes[1].value = "visible";
+      document.getElementById("resume1").attributes[1].value = "hidden";
+      document.getElementById("resume2").attributes[1].value = "hidden";
+    }
   }
-  else if(saison_ === " Saison 2 ")
+  else if(fichierHTML === "GoT.html")
   {
-    document.getElementById("resume2").attributes[1].value = "visible";
-    document.getElementById("resume1").attributes[1].value = "hidden";
-    document.getElementById("resume3").attributes[1].value = "hidden";
-    document.getElementById("resume4").attributes[1].value = "hidden";
-    document.getElementById("resume5").attributes[1].value = "hidden";
+    if(saison_ === " Saison 1 ")
+    {
+      document.getElementById("resume1").attributes[1].value = "visible";
+      document.getElementById("resume2").attributes[1].value = "hidden";
+      document.getElementById("resume3").attributes[1].value = "hidden";
+      document.getElementById("resume4").attributes[1].value = "hidden";
+      document.getElementById("resume5").attributes[1].value = "hidden";
+    }
+    else if(saison_ === " Saison 2 ")
+    {
+      document.getElementById("resume2").attributes[1].value = "visible";
+      document.getElementById("resume1").attributes[1].value = "hidden";
+      document.getElementById("resume3").attributes[1].value = "hidden";
+      document.getElementById("resume4").attributes[1].value = "hidden";
+      document.getElementById("resume5").attributes[1].value = "hidden";
+    }
+    else if(saison_ === " Saison 3 ")
+    {
+      document.getElementById("resume3").attributes[1].value = "visible";
+      document.getElementById("resume1").attributes[1].value = "hidden";
+      document.getElementById("resume2").attributes[1].value = "hidden";
+      document.getElementById("resume4").attributes[1].value = "hidden";
+      document.getElementById("resume5").attributes[1].value = "hidden";
+    }
+    else if(saison_ === " Saison 4 ")
+    {
+      document.getElementById("resume4").attributes[1].value = "visible";
+      document.getElementById("resume1").attributes[1].value = "hidden";
+      document.getElementById("resume2").attributes[1].value = "hidden";
+      document.getElementById("resume3").attributes[1].value = "hidden";
+      document.getElementById("resume5").attributes[1].value = "hidden";
+    }
+    else if(saison_ === " Saison 5 ")
+    {
+      document.getElementById("resume5").attributes[1].value = "visible";
+      document.getElementById("resume1").attributes[1].value = "hidden";
+      document.getElementById("resume2").attributes[1].value = "hidden";
+      document.getElementById("resume3").attributes[1].value = "hidden";
+      document.getElementById("resume4").attributes[1].value = "hidden";
+    }
   }
-  else if(saison_ === " Saison 3 ")
+  else if(fichierHTML === "HoC.html")
   {
-    document.getElementById("resume3").attributes[1].value = "visible";
-    document.getElementById("resume1").attributes[1].value = "hidden";
-    document.getElementById("resume2").attributes[1].value = "hidden";
-    document.getElementById("resume4").attributes[1].value = "hidden";
-    document.getElementById("resume5").attributes[1].value = "hidden";
+    if(saison_ === " Saison 1 ")
+    {
+      document.getElementById("resume1").attributes[1].value = "visible";
+      document.getElementById("resume2").attributes[1].value = "hidden";
+    }
+    else if(saison_ === " Saison 2 ")
+    {
+      document.getElementById("resume2").attributes[1].value = "visible";
+      document.getElementById("resume1").attributes[1].value = "hidden";
+    }
+
   }
-  else if(saison_ === " Saison 4 ")
-  {
-    document.getElementById("resume4").attributes[1].value = "visible";
-    document.getElementById("resume1").attributes[1].value = "hidden";
-    document.getElementById("resume2").attributes[1].value = "hidden";
-    document.getElementById("resume3").attributes[1].value = "hidden";
-    document.getElementById("resume5").attributes[1].value = "hidden";
-  }
-  else if(saison_ === " Saison 5 ")
-  {
-    document.getElementById("resume5").attributes[1].value = "visible";
-    document.getElementById("resume1").attributes[1].value = "hidden";
-    document.getElementById("resume2").attributes[1].value = "hidden";
-    document.getElementById("resume3").attributes[1].value = "hidden";
-    document.getElementById("resume4").attributes[1].value = "hidden";
-  }
+
 }
