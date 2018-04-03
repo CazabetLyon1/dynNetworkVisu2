ef41b236d9c206bdfd9dd1c1cd67cc4ebf6478c6
Thomas <tlabrux@gmail.com>
2018-04-03 14:58:50 +0200

    hahahahahaha :)

--- js/script_D3.js
+++ js/script_D3.js
@@ -4,10 +4,14 @@ height = +svg.attr("height");
 
 var color = d3v4.scaleOrdinal(d3v4.schemeCategory20);
 
+
+
 var simulation = d3v4.forceSimulation()
 .force("link", d3v4.forceLink().id(function(d) { return d.id; }))
-.force("charge", d3v4.forceManyBody().strength(-150).distanceMax(300).distanceMin(50))
+.force("charge", d3v4.forceManyBody())
 .force("center", d3v4.forceCenter(width / 2, height / 2));
+
+
 
 
 var link = svg.append("g").selectAll(".link"),
@@ -145,6 +149,9 @@ function updateGraph(nodes, edges) {
 
 
 function update(nodes, links) {
+
+
+
   link = link.data(link, function(d) { return d.source.id + "-" + d.target.id; });
   link.exit().remove();
 
@@ -261,132 +268,14 @@ function update(nodes, links) {
     }
   });
 
-  simulation.nodes(nodes);
-  simulation.force("link").links(links);
+  // simulation.nodes(nodes);
+  
   simulation.alpha(1).restart();
+  simulation.force("link").links(links);
+  simulation.force("center", d3v4.forceCenter(width / 2, height / 2)).strength(1);
 
 
 }
-
-// function update(nodes, edges) {
-//   link = link.data(link, function(d) { return d.source.id + "-" + d.target.id; });
-//   link.exit().remove();
-//   link = svg.append("g")
-//   .attr("class", "links")
-//   .selectAll("line")
-//   .data(edges)
-//   .enter().append("line")
-//   .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
-
-//   node = node.data(node, function(d) { return d.id;});
-//   node.exit().remove();
-//   node = svg.append("g")
-//   .attr("class", "nodes")
-//   .selectAll("circle")
-//   .data(nodes)
-//   .enter().append("circle")
-//   .attr("r", function(d){return tailleDuNoeudVoisin(d);})
-//   .attr("fill", function(d) { return couleurDuNoeudVoisin(d);})
-//   .call(d3v4.drag()
-//     .on("start", dragstarted)
-//     .on("drag", dragged)
-//     .on("end", dragended));
-
-//   node.append("title")
-//   .text(function(d) { return d.label; });
-
-//   node.append("image")
-//   .attr("xlink:href", function(d) { return d.image; });
-
-//   node.append("text")
-//   .text(function(d) { return d.label; });
-
-//   node.append("id")
-//   .text(function(d) { return d.id; });
-
-//   node.on("click", function()
-//   {
-//     var chemin = document.location.pathname;
-//     var fichierHTML = chemin.substring(chemin.lastIndexOf( "/" )+1);
-
-//     var id = this.childNodes[3].innerHTML;
-//     var name = this.childNodes[2].innerHTML;
-//     default_image = "Donnees/Photos/Default.png";
-
-//     listePhoto = listePhoto.sort();
-//     //if(listePhoto[0] == "") listePhoto.splice(0, 1);
-//     //compter le nombre de photos
-
-//     if(fichierHTML === "BB.html")
-//     {
-//       var i;
-//       var photoOK = false;
-//       for (i = 0; i < 20; i++)
-//       {
-//         var photo = listePhoto[i];
-//         if ((name + ".jpg") === photo)
-//         {
-//           image = "Donnees/Photos/BB/" + photo;
-//           document.getElementById('img').attributes[1].value = image;
-//           photoOK = true;
-//         }
-//       }
-//       if (photoOK === false)
-//       {
-//         document.getElementById('img').attributes[1].value = default_image;
-//       }
-
-//       document.getElementById('name').innerHTML = name;
-//     }
-//     else if(fichierHTML === "GoT.html")
-//     {
-//       var i;
-//       var photoOK = false;
-//       for (i = 0; i < 20; i++)
-//       {
-//         var photo = listePhoto[i];
-//         if ((name + ".jpg") === photo)
-//         {
-//           image = "Donnees/Photos/GoT/" + photo;
-//           document.getElementById('img').attributes[1].value = image;
-//           photoOK = true;
-//         }
-//       }
-//       if (photoOK === false)
-//       {
-//         document.getElementById('img').attributes[1].value = default_image;
-//       }
-
-//       document.getElementById('name').innerHTML = name;
-//     }
-//     else if(fichierHTML === "HoC.html")
-//     {
-//       var i;
-//       var photoOK = false;
-//       for (i = 0; i < 20; i++)
-//       {
-//         var photo = listePhoto[i];
-//         if ((name + ".jpg") === photo)
-//         {
-//           image = "Donnees/Photos/HoC/" + photo;
-//           document.getElementById('img').attributes[1].value = image;
-//           photoOK = true;
-//         }
-//       }
-//       if (photoOK === false)
-//       {
-//         document.getElementById('img').attributes[1].value = default_image;
-//       }
-
-//       document.getElementById('name').innerHTML = name;
-//     }
-//   });
-
-
-//   simulation.nodes(nodes);
-//   simulation.force("link").links(edges);
-//   simulation.alpha(1).restart();
-// }
 
 function ticked() {
   link
@@ -401,9 +290,7 @@ function ticked() {
 }
 
 function dragstarted(d) {
-  if (!d3v4.event.active) simulation.alphaTarget(0.3).restart();
-  d.fx = d.x;
-  d.fy = d.y;
+   if (!d3.event.active) simulation.alphaTarget(0);
 }
 
 function dragged(d) {
